09be1eb0fd8603db8994b798be321b2304c3d105
suuchan <aynsuu25@gmail.com>
2022-01-03 04:02:12 +0000

    User編集&show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   skip_before_action :require_sign_in!#, only: [:new, :create]
   before_action :authenticate_with_http_basic
-  before_action :ensure_correct_user, only: [:show]
+  before_action :ensure_correct_user, only: [:show, :edit]
   
   def new
     @user = User.new
@@ -26,6 +26,19 @@ class UsersController < ApplicationController
   
   def show
     @user=User.find(params[:id])
+  end
+  
+  def edit
+    @user=User.find(params[:id])
+  end
+  
+  def update
+     @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
   
   def ensure_correct_user
